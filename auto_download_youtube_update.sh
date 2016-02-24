cb09d7061f3f92b98472e58de90db192fe23db5f
neo.alienson <neo@orz.hk>
2016-02-24 17:26:39 +0800

    init

---
+++ auto_download_youtube_update.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# options for youtube-dl, e.g., --proxy
+OPTS='--max-downloads 10'
+
+declare -A URLS
+
+# Sample watch list.
+# Folders are created with key name
+URLS[cnet_update]=https://www.youtube.com/channel/SWYI9iD9kH0x0
+URLS[cnet_news]=https://www.youtube.com/channel/SWaHzZo6fkq8Y
+URLS[cracking_open]=https://www.youtube.com/channel/SW4YaZBeFDgzA
+
+for KEY in ${!URLS[@]};
+do
+  echo $KEY
+  mkdir -p $KEY
+  cd $KEY
+  youtube-dl $OPTS ${URLS[$KEY]}
+  cd ..
+done
